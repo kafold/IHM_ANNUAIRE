18e6a696ca9e18ab743e0223da937032d34d1b51
Lafortune Pierre <lafortune.pierre@etu.unice.fr>
2015-04-18 09:36:24 +0200

    Recup du prof

---
+++ app/scripts/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pooIhmExemplesApp
+ * @description
+ * # pooIhmExemplesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pooIhmExemplesApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/users' , {
+        templateUrl: 'views/Users/list.html',
+        controller: 'UsersCtrl'
+      })
+      .when('/users/:userId', {
+        templateUrl: 'views/Users/show.html',
+        controller: 'UsersCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/users.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pooIhmExemplesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pooIhmExemplesApp
+ */
+angular.module('pooIhmExemplesApp')
+  .controller('UsersCtrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $http.get('http://poo-ihm-2015-rest.herokuapp.com/api/Users')
+      .success(function(data) {
+        $scope.users = data.data;
+      });
+
+    if($routeParams.userId) {
+      $http.get('http://poo-ihm-2015-rest.herokuapp.com/api/Users/' + $routeParams.userId)
+      .success(function(data) {
+        if (data.status == "success") {
+          $scope.currentUser = data.data;
+        }
+      });
+    }
+  }]);
